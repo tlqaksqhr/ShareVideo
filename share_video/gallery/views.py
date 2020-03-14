cd440bb41f41a5606ce69d9c885478a5200eef4e
sim manseop <tlqaksqhr@naver.com>
2020-03-14 23:31:33 +0900

    add views

--- share_video/gallery/views.py
+++ share_video/gallery/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def video_list(request):
+    return HttpResponse("Video List")
+
+def video_upload(request):
+    return HttpResponse("Video Upload")
