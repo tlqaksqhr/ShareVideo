333dfbcc9be502c43648c7fca78aeb8c31a04a9e
sim manseop <tlqaksqhr@naver.com>
2020-03-14 23:30:09 +0900

    fix model

--- share_video/gallery/models.py
+++ share_video/gallery/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     link = models.TextField()
     desc = models.TextField()
-    writer = models.ForeignKey(User, related_name='posts')
+    writer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
 
 class Comment(models.Model):
     desc = models.TextField()
-    writer = models.ForeignKey(User, related_name='comments')
+    writer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
