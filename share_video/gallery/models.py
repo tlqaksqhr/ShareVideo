b5ee944c0da990f1f73b8193e8a14ffc1e6826b0
sim manseop <tlqaksqhr@naver.com>
2020-03-17 23:42:10 +0900

    add models

--- share_video/gallery/models.py
+++ share_video/gallery/models.py
@@ -5,8 +5,15 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     link = models.TextField()
     desc = models.TextField()
-    writer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    write_date = models.DateField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE,related_name='posts')
+    favorites = models.ForeignKey(Favorites, on_delete=models.CASCADE,related_name='posts')
 
 class Comment(models.Model):
     desc = models.TextField()
     writer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
+    write_date = models.DateField()
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='favorites')
